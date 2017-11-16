bf5cd1f8442d0dc7edf533918b90894ae2d82d5b
Omar López <omarjlg1893@gmail.com>
2017-11-16 13:49:16 -0430

    init repo

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+  <header class="points-info"></header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss" >
+body{
+  padding: 0px;
+  margin: 0px;
+}
+
+.points-info{
+  background-color: blue;
+  height: 100px;
+  width: 100%;
+}
+</style>
